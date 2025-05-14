4d06af519cb06e8f1e1e79b4eac2fe41cd29ae94
Suchita Dabir <dabir.s@northeastern.edu>
2025-05-14 11:19:16 -0400

    DP-5 homework 1

--- src/WordBreak.java
+++ src/WordBreak.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class WordBreak {
+import java.util.HashSet;
+import java.util.List;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        HashSet<String> set = new HashSet<>(wordDict);
+        int n = s.length();
+
+        boolean[] dp = new boolean[n+1];
+
+        dp[0] = true;
+
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<i ; j++){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
 }
